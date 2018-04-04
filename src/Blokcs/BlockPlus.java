81a5d3d56130876668e5b12f878db6b4b4b7b9fe
Sufu <sufu@sufu-X555LF>
2018-04-04 14:32:40 +0200

    first children

---
+++ src/Blokcs/BlockPlus.java
@@ -0,0 +1,17 @@
+package Blokcs;
+
+public class BlockPlus extends Block {
+
+	BlockPlus(BlockType[] in) {
+		super(in);
+	}
+	
+	public BlockType[] execute() {
+		double[] tmp = {IN[0].getValues()[0] + IN[1].getValues()[0]};
+		BlockType result = new BlockType(tmp, IN[0].getNames());
+		OUT = new BlockType[]{result};
+		return OUT;
+		
+	}
+
+}
--- src/Blokcs/BlockType.java
+++ src/Blokcs/BlockType.java
@@ -1,3 +1,4 @@
+package Blokcs;
 
 public class BlockType {
 	
