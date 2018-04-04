1423dffccbd871ffcd9872d9c179ab4624075f12
Jiří Furda <Furdys@users.noreply.github.com>
2018-04-05 01:56:48 +0200

    findNonDependentBlock

--- src/ija/core/Scheme.java
+++ src/ija/core/Scheme.java
@@ -5,6 +5,8 @@
  */
 package ija.core;
 
+import blocks.*;
+
 import java.util.ArrayList;
 
 /**
@@ -14,7 +16,7 @@ import java.util.ArrayList;
 public class Scheme {
     private String name;
     private ArrayList<Block> blocks = new ArrayList<Block>();
-    private ArrayList<Block> visitedBlocks = new ArrayList<Block>();
+//    private ArrayList<Block> dependentBlocks = new ArrayList<Block>();
     private Block nextBlock;
     
     private int maxBlocks = 420;    // @todo Change later
@@ -62,20 +64,54 @@ public class Scheme {
     
     private boolean executeBlock(Block block)
     {
-        // --- Check for loops ---
-        if(this.visitedBlocks.contains(block))
+        // --- Check for null block ---
+        if(block == null)
         {
-            System.out.println("ERROR: Loop detected!");
-            // @todo die
+            System.err.println("ERROR: Tried to execute null block");
+            return false;        
         }
         
-        // @todo Check if parent blocks are calculated?
+        block = this.findNonDependentBlock(block);
+
+        // --- Check for loops --- 
+        if(inputPort.getBlock().wasExecuted == true)) // @todo This should be in Block class
+        {
+            System.err.println("ERROR: Loop detected (Tried to execute one block twice)!");
+            return false;
+        }
         
         // --- Execute calculation ---
         block.execute();
-        
-        visitedBlocks.add(block);
     }
+    
+    private Block findNonDependentBlock(Block block)
+    {
+        // --- Check if block can be executed ---
+        for(Port port : block.getIN())    // Check every input port
+        {
+            Port inputPort = port.getConnectedPort();
+            
+            // --- Check if port is not connected ---
+            if(inputPort == null)
+            {
+                // This should never happen, user must fill every non-connected port before start
+                System.err.println("Scheme.checkDependency: Block is dependent on user input");
+                System.exit(1);
+            }
+            
+            // --- Check for block with no value ---
+            if(inputPort.getBlock().wasExecuted() == false)
+            {
+                // That means this block is waiting on inputPort's block calculation
+                //this.dependentBlocks.add(block); @ todo Save visited blocks to reduce redundant search
+                return this.findNonDependentBlock(inputPort.getBlock()); // Use recursive call (ask if inputPort's block is depended)
+            }
+        }
+        
+        // --- Block is not depended & can be executed ---
+        return block;
+    }
+    
     
     private double getUserInput()
     {
