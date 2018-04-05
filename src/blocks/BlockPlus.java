2bf1aaf6f657f35a0b0a587c75b680bbc1d22b27
Sufu <sufu@sufu-X555LF>
2018-04-05 13:21:36 +0200

    fixed execute

--- src/blocks/BlockPlus.java
+++ src/blocks/BlockPlus.java
@@ -1,20 +1,21 @@
 package blocks;
 
-public class BlockPlus extends Block {
+public class BlockPlus extends Block 
+{
 
-	public BlockPlus(Port[] in) {
+	public BlockPlus(Port[] in)
+	{
 		super(in);
 	}
 	
-        @Override
-	public Port[] execute() {
-		String[] arr = IN[0].getNames().toArray(new String[IN[0].getNames().size()]);
-                String[] zabijuPetera = IN[1].getNames().toArray(new String[IN[1].getNames().size()]);
-		Port result = new Port(arr[0]);
-		result.setValue(arr[0], IN[0].getValue(arr[0]) + IN[1].getValue(zabijuPetera[0]));
-                //result.getValue(arr[0]);
+	@Override
+	public Port[] execute() 
+	{
+		String[] firstOpName = IN[0].getNames().toArray(new String[IN[0].getNames().size()]);
+		String[] secondOpName = IN[1].getNames().toArray(new String[IN[1].getNames().size()]);
+		Port result = new Port(firstOpName[0]);
+		result.setValue(firstOpName[0], IN[0].getValue(firstOpName[0]) + IN[1].getValue(secondOpName[0]));
 		OUT = new Port[]{result};
-                //OUT[0].getValue("x");
 		return OUT;
 		
 	}
