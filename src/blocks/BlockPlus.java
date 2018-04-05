525a8e4bb18bf1608e628cdd3a929f9f3f7228ed
Jiří Furda <Furdys@users.noreply.github.com>
2018-04-05 03:55:32 +0200

    Test01 working

--- src/blocks/BlockPlus.java
+++ src/blocks/BlockPlus.java
@@ -9,9 +9,12 @@ public class BlockPlus extends Block {
         @Override
 	public Port[] execute() {
 		String[] arr = IN[0].getNames().toArray(new String[IN[0].getNames().size()]);
-		Port result = new Port();
-		result.setValue(arr[0], IN[0].getValue(arr[0]) + IN[1].getValue(arr[0]));
+                String[] zabijuPetera = IN[1].getNames().toArray(new String[IN[1].getNames().size()]);
+		Port result = new Port(arr[0]);
+		result.setValue(arr[0], IN[0].getValue(arr[0]) + IN[1].getValue(zabijuPetera[0]));
+                //result.getValue(arr[0]);
 		OUT = new Port[]{result};
+                //OUT[0].getValue("x");
 		return OUT;
 		
 	}
